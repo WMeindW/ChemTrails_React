6dda440dcb82016d1025c002445bfc1fce3b67c4
Meind <105383135+WMeindW@users.noreply.github.com>
2023-09-13 11:18:24 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,74 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
+    function click(event: React.MouseEvent<HTMLDivElement>) {
+
+    }
+
     return <div className={"row element-row"}>
-        <div className={"card element-container"}>
-            <div className={"element-container-number"}>1</div>
-            <div className={"element-container-header"}>H</div>
-            <div className={"element-container-weight"}>1.008</div>
+        <div onClick={(e) => click(e)} className={"card element-container-blue"}>
+            <div className={"element-container-number-blue"}>1</div>
+            <div className={"element-container-header-blue"}>H</div>
+            <div className={"element-container-weight-blue"}>1.008</div>
         </div>
-        <div className={"card element-container"}>
-            <div className={"element-container-number"}>1</div>
-            <div className={"element-container-header"}>H</div>
-            <div className={"element-container-weight"}>1.008</div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div onClick={(e) => click(e)} className={"card element-container-purple"}>
+            <div className={"element-container-number-purple"}>2</div>
+            <div className={"element-container-header-purple"}>He</div>
+            <div className={"element-container-weight-purple"}>4.0026</div>
+        </div>
+        <div onClick={(e) => click(e)} className={"card element-container-pink"}>
+            <div className={"element-container-number-pink"}>3</div>
+            <div className={"element-container-header-pink"}>Li</div>
+            <div className={"element-container-weight-pink"}>6.94</div>
+        </div>
+        <div onClick={(e) => click(e)} className={"card element-container-orange"}>
+            <div className={"element-container-number-orange"}>4</div>
+            <div className={"element-container-header-orange"}>Be</div>
+            <div className={"element-container-weight-orange"}>9.0122</div>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
+        </div>
+        <div className={"card element-container-transparent"}>
         </div>
     </div>;
 }
