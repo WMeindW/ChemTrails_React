6ae6d0068b81fe183b17ab536d5577cce4019a14
Meind <105383135+WMeindW@users.noreply.github.com>
2023-09-13 10:10:43 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return;
+export default function App() {
+    return <div className={"row element-row"}>
+        <div className={"card element-container"}>
+            <div className={"element-container-number"}>1</div>
+            <div className={"element-container-header"}>H</div>
+            <div className={"element-container-weight"}>1.008</div>
+        </div>
+        <div className={"card element-container"}>
+            <div className={"element-container-number"}>1</div>
+            <div className={"element-container-header"}>H</div>
+            <div className={"element-container-weight"}>1.008</div>
+        </div>
+    </div>;
 }
-
-export default App;
